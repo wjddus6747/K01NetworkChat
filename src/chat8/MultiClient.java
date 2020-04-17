e2526570e0e167f0850d2ff82da47bda0c42862d
wjddus6747 <wjddus6747@gmail.com>
2020-04-17 16:31:43 +0900

    0417

--- src/chat8/MultiClient.java
+++ src/chat8/MultiClient.java
@@ -7,7 +7,7 @@ public class MultiClient
 {
    public static void main(String[] args)
    {
-      System.out.print("이름을 입력하세요 : ");
+      System.out.print("이름을 입력하세요 : ");   
       Scanner scanner = new Scanner(System.in);
       String s_name = scanner.nextLine();
 
